0736c2dd805a52c3ed59584a1ab91b002ae410a1
Claire <toshcr1@student.op.ac.nz>
2019-10-23 01:02:07 +1300

    New setting pages

---
+++ GazeToolBar/keyboardSettings.Designer.cs
@@ -0,0 +1,378 @@
+﻿namespace GazeToolBar
+{
+    partial class keyboardSettings
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelSaveAndCancel = new System.Windows.Forms.Panel();
+            this.pnlCancel = new System.Windows.Forms.Panel();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.pnlSave = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnAutoStart = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panelSaveAndCancel.SuspendLayout();
+            this.pnlCancel.SuspendLayout();
+            this.pnlSave.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panelSaveAndCancel
+            // 
+            this.panelSaveAndCancel.Controls.Add(this.pnlCancel);
+            this.panelSaveAndCancel.Controls.Add(this.pnlSave);
+            this.panelSaveAndCancel.Location = new System.Drawing.Point(288, 441);
+            this.panelSaveAndCancel.Margin = new System.Windows.Forms.Padding(2);
+            this.panelSaveAndCancel.Name = "panelSaveAndCancel";
+            this.panelSaveAndCancel.Size = new System.Drawing.Size(782, 164);
+            this.panelSaveAndCancel.TabIndex = 36;
+            // 
+            // pnlCancel
+            // 
+            this.pnlCancel.Controls.Add(this.btnCancel);
+            this.pnlCancel.Location = new System.Drawing.Point(626, 2);
+            this.pnlCancel.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlCancel.Name = "pnlCancel";
+            this.pnlCancel.Size = new System.Drawing.Size(154, 155);
+            this.pnlCancel.TabIndex = 22;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.Transparent;
+            this.btnCancel.FlatAppearance.BorderSize = 3;
+            this.btnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCancel.Font = new System.Drawing.Font("Arial", 24F);
+            this.btnCancel.ForeColor = System.Drawing.Color.White;
+            this.btnCancel.Location = new System.Drawing.Point(2, 2);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(2);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(150, 150);
+            this.btnCancel.TabIndex = 24;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            // 
+            // pnlSave
+            // 
+            this.pnlSave.Controls.Add(this.btnSave);
+            this.pnlSave.Location = new System.Drawing.Point(5, 2);
+            this.pnlSave.Margin = new System.Windows.Forms.Padding(2);
+            this.pnlSave.Name = "pnlSave";
+            this.pnlSave.Size = new System.Drawing.Size(154, 155);
+            this.pnlSave.TabIndex = 22;
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.Transparent;
+            this.btnSave.FlatAppearance.BorderSize = 3;
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Arial", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.ForeColor = System.Drawing.Color.White;
+            this.btnSave.Location = new System.Drawing.Point(2, 2);
+            this.btnSave.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(150, 150);
+            this.btnSave.TabIndex = 23;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(208, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 31);
+            this.label1.TabIndex = 37;
+            this.label1.Text = "abc";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(418, 110);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 31);
+            this.label2.TabIndex = 38;
+            this.label2.Text = "123";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(611, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 31);
+            this.label3.TabIndex = 39;
+            this.label3.Text = "àbç";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(1000, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(186, 31);
+            this.label4.TabIndex = 40;
+            this.label4.Text = "AutoComplete";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(773, 110);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 31);
+            this.label5.TabIndex = 41;
+            this.label5.Text = "esp";
+            // 
+            // btnAutoStart
+            // 
+            this.btnAutoStart.BackColor = System.Drawing.Color.Black;
+            this.btnAutoStart.FlatAppearance.BorderSize = 5;
+            this.btnAutoStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnAutoStart.ForeColor = System.Drawing.Color.White;
+            this.btnAutoStart.Location = new System.Drawing.Point(199, 194);
+            this.btnAutoStart.Name = "btnAutoStart";
+            this.btnAutoStart.Size = new System.Drawing.Size(80, 75);
+            this.btnAutoStart.TabIndex = 42;
+            this.btnAutoStart.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.FlatAppearance.BorderSize = 5;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(199, 323);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(80, 75);
+            this.button1.TabIndex = 43;
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.FlatAppearance.BorderSize = 5;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(397, 194);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(80, 75);
+            this.button2.TabIndex = 44;
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Black;
+            this.button3.FlatAppearance.BorderSize = 5;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(397, 323);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(80, 75);
+            this.button3.TabIndex = 45;
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Black;
+            this.button4.FlatAppearance.BorderSize = 5;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.Color.White;
+            this.button4.Location = new System.Drawing.Point(589, 194);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(80, 75);
+            this.button4.TabIndex = 46;
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Black;
+            this.button5.FlatAppearance.BorderSize = 5;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.Color.White;
+            this.button5.Location = new System.Drawing.Point(589, 323);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 75);
+            this.button5.TabIndex = 47;
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Black;
+            this.button6.FlatAppearance.BorderSize = 5;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.Color.White;
+            this.button6.Location = new System.Drawing.Point(751, 194);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(80, 75);
+            this.button6.TabIndex = 48;
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.Black;
+            this.button7.FlatAppearance.BorderSize = 5;
+            this.button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button7.ForeColor = System.Drawing.Color.White;
+            this.button7.Location = new System.Drawing.Point(751, 323);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(80, 75);
+            this.button7.TabIndex = 49;
+            this.button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.Black;
+            this.button8.FlatAppearance.BorderSize = 5;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button8.ForeColor = System.Drawing.Color.White;
+            this.button8.Location = new System.Drawing.Point(1031, 194);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(80, 75);
+            this.button8.TabIndex = 50;
+            this.button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.Black;
+            this.button9.FlatAppearance.BorderSize = 5;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button9.ForeColor = System.Drawing.Color.White;
+            this.button9.Location = new System.Drawing.Point(1031, 323);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(80, 75);
+            this.button9.TabIndex = 51;
+            this.button9.UseVisualStyleBackColor = false;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(79, 210);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 31);
+            this.label6.TabIndex = 52;
+            this.label6.Text = "On";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(79, 354);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 31);
+            this.label7.TabIndex = 53;
+            this.label7.Text = "Off";
+            // 
+            // keyboardSettings
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(1434, 616);
+            this.ControlBox = false;
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnAutoStart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panelSaveAndCancel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.Name = "keyboardSettings";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "keyboardSettings";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.panelSaveAndCancel.ResumeLayout(false);
+            this.pnlCancel.ResumeLayout(false);
+            this.pnlSave.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelSaveAndCancel;
+        private System.Windows.Forms.Panel pnlCancel;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Panel pnlSave;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnAutoStart;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
---
+++ GazeToolBar/keyboardSettings.cs
@@ -0,0 +1,32 @@
+﻿using EyeXFramework.Forms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GazeToolBar
+{
+    public partial class keyboardSettings : Form
+    {
+        private HomeSettings home;
+        private static FormsEyeXHost eyeXHost;
+        private Form1 form1;
+        public keyboardSettings(HomeSettings home, Form1 form1, FormsEyeXHost EyeXHost)
+        {
+            eyeXHost = EyeXHost;
+            InitializeComponent();
+            this.home = home;
+            this.form1 = form1;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
